41b83dd834fffefc8c3aab5917659ce00015089b
Eric Edmond <eedmond@umich.edu>
2020-12-21 09:38:21 -0800

    Implement enable/disable of openings

--- src/OpeningsTrie.tsx
+++ src/OpeningsTrie.tsx
@@ -37,29 +37,109 @@ export class OpeningsTrie {
   
     // Returns true if move is part of a valid opening
     isValidMove(move: ShortMove) {
-      return this.getCurrentTrieNode().nextMoves.has(this.moveToString(move));
+      return this.getCurrentTrieNode().nextMoves
+        .get(this.moveToString(move))?.isActive;
     }
   
     // Returns a computer move selected randomly from the available openings
     getNextMove(): ShortMove {
-      let moves = Array.from(this.getCurrentTrieNode().nextMoves.keys());
-      return this.stringToMove(moves[Math.floor(Math.random() * moves.length)]);
+      const nextMoves = this.getCurrentTrieNode().nextMoves;
+
+      let moveStrings = Array.from(this.getCurrentTrieNode().nextMoves.keys());
+      moveStrings = moveStrings.filter(move => nextMoves.get(move)!.isActive);
+      return this.stringToMove(moveStrings[Math.floor(Math.random() * moveStrings.length)]);
     }
   
     // Returns true if there are any remaining moves that follow saved openings lines
     hasMovesToMake() {
-      return this.getCurrentTrieNode().nextMoves.size > 0;
+      const nextMoves = Array.from(this.getCurrentTrieNode().nextMoves.values());
+      return nextMoves.filter(node => node.isActive).length > 0;
     }
   
     // Returns the list of (incomplete) openings that are possible from the current move sequence
     getPossibleOpeningsFromCurrentPosition(): Opening[] {
-      let currentNode = this.getCurrentTrieNode();
+      const currentNode = this.getCurrentTrieNode();
       return this.getPossibleOpeningsUnderNode(currentNode);
     }
   
     // Returns the list of openings that are just completed from the current move sequence
     getCompletedOpeningsFromCurrentPosition(): Opening[] {
-      return this.getCurrentTrieNode().openings;
+      return this.getCurrentTrieNode().openings.filter(op => op.isActive);
+    }
+
+    enableOpening(opening: Opening) {
+      this.setOpeningEnabledState(opening, true);
+    }
+
+    disableOpening(opening: Opening) {
+      this.setOpeningEnabledState(opening, false);
+    }
+
+    private setOpeningEnabledState(opening: Opening, newisOpeningEnabled: boolean) {
+      const nodeForOpening = this.getEndNodeForOpening(opening, newisOpeningEnabled);
+
+      // Find the specific opening at this node and set its active state
+      nodeForOpening.openings.find(op => (op.name === opening.name) &&
+        (op.eco === opening.eco))!.isActive = newisOpeningEnabled;
+
+      // Set the active state of the entire node
+      nodeForOpening.isActive = newisOpeningEnabled ||
+        this.doesNodeHaveAnActiveOpening(nodeForOpening) ||
+        this.doesNodeHaveAnActiveNextNode(nodeForOpening);
+
+      if (!nodeForOpening.isActive) {
+        this.validateDisabledNodesToOpening(opening);
+      }
+    }
+
+    // Returns the node corresponding to the end of the specified opening.
+    // Optionally sets all nodes to this opening as active. This is useful
+    // if the opening is becoming active since all nodes leading to it then
+    // must be set to active as well.
+    private getEndNodeForOpening(opening: Opening, setAllToActive?: boolean): TrieNode {
+      let currentNode = this.rootNode;
+
+      opening.moves.forEach(move => {
+        currentNode = currentNode.nextMoves.get(this.moveToString(move))!;
+
+        // If we're getting a node for an opening we want to set to active,
+        // then all nodes leading to this opening should also be set to active.
+        if (setAllToActive?.valueOf()) {
+          currentNode.isActive = true;
+        }
+      });
+
+      return currentNode;
+    }
+
+    private validateDisabledNodesToOpening(opening: Opening) {
+      const nodeStack = [this.rootNode];
+
+      opening.moves.forEach(move => {
+        const bottomNode = nodeStack[nodeStack.length - 1];
+        nodeStack.push(bottomNode.nextMoves.get(this.moveToString(move))!);
+      });
+
+      while (nodeStack.length > 0) {
+        const bottomNode = nodeStack.pop()!;
+
+        bottomNode.isActive = this.doesNodeHaveAnActiveOpening(bottomNode) ||
+          this.doesNodeHaveAnActiveNextNode(bottomNode);
+
+        // Stop working up the stack when a node is active since this is just chaining disables
+        if (bottomNode.isActive) {
+          break;
+        }
+      }
+    }
+    
+    private doesNodeHaveAnActiveOpening(node: TrieNode): boolean {
+      return node.openings.find(op => op.isActive) !== undefined;
+    }
+
+    private doesNodeHaveAnActiveNextNode(node: TrieNode): boolean {
+      return Array.from(node.nextMoves.values()).find(nextNode =>
+        nextNode.isActive) !== undefined;
     }
 
     // Utility that returns the trie node given the current state of the board
@@ -83,8 +163,11 @@ export class OpeningsTrie {
           break;
         }
 
-        openings.push(...iterator.value.openings);
-        openings.push(...this.getPossibleOpeningsUnderNode(iterator.value));
+        openings.push(...(iterator.value.openings).filter(op => op.isActive));
+
+        if (iterator.value.isActive) {
+          openings.push(...this.getPossibleOpeningsUnderNode(iterator.value));
+        }
       } while(!iterator.done);
 
       return openings;
